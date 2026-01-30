772d1e041eaecaa7014bfa80f276bbb70c8ebe4a
MedvedevDev <a95.medvedev@gmail.com>
2026-01-30 22:40:06 +0100

    search bar v.2

--- src/components/css-figures/ExpandingSearchBar.jsx
+++ src/components/css-figures/ExpandingSearchBar.jsx
@@ -1,9 +1,14 @@
 export function ExpandingSearchBar() {
   return (
-    <input
-      type="search"
-      placeholder="Search..."
-      className=" bg-white/10 text-white w-[6%] leading-[2] px-2 border-2 border-gray-700 rounded-lg focus:w-[20%] focus:border-pink-600 transition-all duration-300 ease-in-out origin-left  focus:outline-none"
-    />
+    <div className="relative w-[20rem]">
+      <input
+        type="search"
+        placeholder="Search..."
+        className="absolute left-0 bg-white/10 text-white w-40 leading-[2] px-2 border-2
+       border-gray-700 rounded-lg focus:w-80
+        focus:border-pink-600 transition-[width] duration-300 ease-in-out
+        focus:outline-none"
+      />{" "}
+    </div>
   );
 }
